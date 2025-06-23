60ea82173a8a4c701fafb3401558ac325c95a4c1
cgw <ruddnjs1865@naver.com>
2025-06-23 18:01:12 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Outlet, useLocation } from "react-router-dom";
-import Header from "./components/Header";
+import Header from "@/components/Header";
 import { motion } from "motion/react";
 
 export default function App() {
