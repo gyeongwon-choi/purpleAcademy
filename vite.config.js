eef14fcd6e8b1743c503ea3368d0361d801fd19b
gyeongwon <ruddnjs1865@naver.com>
2025-07-22 02:33:43 +0900

    ㅇㅇ

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: process.env.VITE_DIRECTORY,
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: env.VITE_DIRECTORY,
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  server: {
-    host: '0.0.0.0', // 외부 IP 접속 허용
-    port: 5555, // 원하는 포트
-    allowedHosts: ['1f807de4cd20.ngrok-free.app'], // 모든 호스트 허용
-  },
+    server: {
+      host: '0.0.0.0',
+      port: 5555,
+      allowedHosts: ['1f807de4cd20.ngrok-free.app'],
+    },
+  }
 })
